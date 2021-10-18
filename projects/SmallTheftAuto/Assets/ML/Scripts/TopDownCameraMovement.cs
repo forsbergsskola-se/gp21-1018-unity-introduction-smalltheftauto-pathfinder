f7fb322ad26b424543fc183ca0afc9afbab46ffa
joechip125 <52743358+joechip125@users.noreply.github.com>
2021-10-18 14:10:45 +0200

    Update TopDownCameraMovement.cs

--- projects/SmallTheftAuto/Assets/ML/Scripts/TopDownCameraMovement.cs
+++ projects/SmallTheftAuto/Assets/ML/Scripts/TopDownCameraMovement.cs
@@ -9,12 +9,9 @@ public class TopDownCameraMovement : MonoBehaviour
     public float zoomPosition;
     
     [SerializeField] private float speed = 20;
-    // Start is called before the first frame update
-    
-    // The lower-left position of the camera, on its current X-Z plane.
+
     [SerializeField] Vector2 minimumLimit = -Vector2.one;
 
-    // The upper-right position of the camera, on its current X-Z plane.
     [SerializeField] Vector2 maximumLimit = Vector2.one;
 
     [SerializeField]  private GameObject thePlayer;
@@ -32,8 +29,7 @@ public class TopDownCameraMovement : MonoBehaviour
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
         
-        
-        // Compute how much movement to apply this frame, in world space
+   
         var offset = new Vector3(horizontal, 0, vertical)
                      * Time.deltaTime * speed;
         
@@ -46,39 +42,5 @@ public class TopDownCameraMovement : MonoBehaviour
 
     }
 
-    // Computes the bounding box that the camera is allowed to be in.
-    Bounds bounds 
-    {
-        get 
-        {
-
-            // We'll create a bounding box that's zero units high, and
-            // positioned at the 
-            
-            //current height of the camera.
-                var cameraHeight = transform.position.y;
-
-            // Figure out the position of the corners of the boxes in
-            // world space
-            Vector3 minLimit = new Vector3(minimumLimit.x,
-                cameraHeight, minimumLimit.y);
-            Vector3 maxLimit = new Vector3(maximumLimit.x,
-                cameraHeight, maximumLimit.y);
-            // Create a new Bounds using these values and return it
-            var newBounds = new Bounds();
-            newBounds.min = minLimit;
-            newBounds.max = maxLimit;
-            return newBounds;
-        }
-    }
-
-
-    // Draw the bounding box.
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        
-        Gizmos.DrawWireCube(bounds.center, bounds.size);
-    }
 
 }
