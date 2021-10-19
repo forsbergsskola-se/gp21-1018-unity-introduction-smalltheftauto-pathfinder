eb7c1b74487fc77cd2bed39da81416917351007a
Wasiq <wasiq.mustafa@student.forsbergsskola.se>
2021-10-19 23:05:57 +0200

    Update CarMovementWM.cs

--- projects/SmallTheftAuto/Assets/WM/Scripts/CarMovementWM.cs
+++ projects/SmallTheftAuto/Assets/WM/Scripts/CarMovementWM.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class CarMovementWM : MonoBehaviour
 {
-    public GameObject Car;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<CarMovementWM>().enabled = false;
     }
 
     // Update is called once per frame
