7927f0ee599037d2ad2945939d40db83028b9d67
Wasiq <wasiq.mustafa@student.forsbergsskola.se>
2021-10-19 23:17:09 +0200

    Create VehicleWM.cs

---
+++ projects/SmallTheftAuto/Assets/WM/Scripts/VehicleWM.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleWM : MonoBehaviour
+{
+    public GameObject player;
+
+    public CarMovementWM CarMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
