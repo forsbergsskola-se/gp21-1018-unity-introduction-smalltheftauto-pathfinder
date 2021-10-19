00cc04a35d7965b5f67c588c0b5a2bad52b6eef1
Wasiq <wasiq.mustafa@student.forsbergsskola.se>
2021-10-19 22:59:35 +0200

    Commit

--- projects/SmallTheftAuto/Assets/WM/Scripts/CarMovementWM.cs
+++ projects/SmallTheftAuto/Assets/WM/Scripts/CarMovementWM.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementWM : MonoBehaviour
+public class CarMovementWM : MonoBehaviour
 {
+    public GameObject Car;
     // Start is called before the first frame update
     void Start()
     {
