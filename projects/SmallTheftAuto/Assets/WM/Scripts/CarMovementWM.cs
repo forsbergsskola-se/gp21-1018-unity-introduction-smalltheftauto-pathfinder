d2eef4eaed12a4b29267be01bba8c3e200d6fc16
Wasiq <wasiq.mustafa@student.forsbergsskola.se>
2021-10-20 14:53:16 +0200

    Commit

--- projects/SmallTheftAuto/Assets/WM/Scripts/CarMovementWM.cs
+++ projects/SmallTheftAuto/Assets/WM/Scripts/CarMovementWM.cs
@@ -7,7 +7,7 @@ public class CarMovementWM : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<CarMovementWM>().enabled = false;
+        
     }
 
     // Update is called once per frame
