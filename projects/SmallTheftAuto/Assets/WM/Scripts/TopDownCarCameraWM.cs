eda4b890bb80ee14721e45799e838661c98caa48
Wasiq <wasiq.mustafa@student.forsbergsskola.se>
2021-10-26 08:54:40 +0200

    Commit

--- projects/SmallTheftAuto/Assets/WM/Scripts/TopDownCarCameraWM.cs
+++ projects/SmallTheftAuto/Assets/WM/Scripts/TopDownCarCameraWM.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class TopDownCarCameraWM : MonoBehaviour
 {
+    public float moveSpeed;
     public GameObject target;
-    public Vector3 offset = new Vector3(0,0,-10);
+    private Transform rigTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigTransform = this.transform.parent;
+
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (target)
-        {
-            transform.position = new Vector3(
-                target.transform.position.x + offset.x,
-                target.transform.position.y + offset.y,
-                target.transform.position.z + offset.z);
+    void FixedUpdate () {
+        if(target == null){
+            return;
         }
+
+        rigTransform.position = Vector3.Lerp(rigTransform.position, target.transform.position, 
+            Time.deltaTime * moveSpeed);
     }
-    
 }
--- projects/SmallTheftAuto/Assets/WM/Scripts/TopDownPlayerCameraWM.cs
+++ projects/SmallTheftAuto/Assets/WM/Scripts/TopDownPlayerCameraWM.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class TopDownPlayerCameraWM : MonoBehaviour
 {
+    public float moveSpeed;
     public GameObject target;
-    public Vector3 offset = new Vector3(0,0,-10);
+    private Transform rigTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigTransform = this.transform.parent;
+
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (target)
-        {
-            transform.position = new Vector3(
-                target.transform.position.x + offset.x,
-                target.transform.position.y + offset.y,
-                target.transform.position.z + offset.z);
+    void FixedUpdate () {
+        if(target == null){
+            return;
         }
+
+        rigTransform.position = Vector3.Lerp(rigTransform.position, target.transform.position, 
+            Time.deltaTime * moveSpeed);
     }
+
 }
