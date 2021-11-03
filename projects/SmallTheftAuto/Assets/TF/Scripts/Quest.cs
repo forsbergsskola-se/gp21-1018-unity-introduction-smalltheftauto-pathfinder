e901fe5d4b9365d633c9227e55e3656c0173d1a6
toofantah <toofan.tah@gmail.com>
2021-11-03 15:02:26 +0300

    Quest System _ Scriptable Object

--- projects/SmallTheftAuto/Assets/TF/Scripts/Quest.cs
+++ projects/SmallTheftAuto/Assets/TF/Scripts/Quest.cs
@@ -106,25 +106,112 @@ public class QuestCompletedEvent : UnityEvent<Quest> { }
 
 #if UNITY_EDITOR
 [CustomEditor(typeof(Quest))]
+
 public class QuestEditor : Editor
 {
+    //serialized property fields for quest infos & rewards
     SerializedProperty m_QuestInfoProperty;
     SerializedProperty m_QuestStatProperty;
 
-    List<string> m_QuestGoalType;
-    SerializedProperty m_QuestGoalListProperty;
+    
+    List<string> m_QuestGoalType;               // List of strings for storing the names different goals 
 
-    [MenuItem("Assets/Quest", priority = 0)]
-    public static void CreateQuest()
+    SerializedProperty m_QuestGoalListProperty; // a serialize feiled for goal list it self
+
+    [MenuItem("Assets/Quest", priority = 0)]    // for showing the menu Item for a quest
+    public static void CreateQuest()           
     {
         var newQuest = CreateInstance<Quest>();
         ProjectWindowUtil.CreateAsset(newQuest, "quest.asset");
 
     }
 
-    private void OnEnable()
+    /// <summary>
+    /// Onenable while be activate right after an instance of a quesrt
+    /// In the methode we gather all the properties and Initialize the field that we created.
+    /// to do this we use .FindProperty method in the serializedObject and pass a parameter to it!
+    /// param -> nameof(the name of the feild we want. ex: Quest.goals);
+    /// </summary>
+    void OnEnable()
     {
+        
+        m_QuestInfoProperty = serializedObject.FindProperty(nameof(Quest.Information));
+        m_QuestStatProperty = serializedObject.FindProperty(nameof(Quest.Reward));
 
+        m_QuestGoalListProperty = serializedObject.FindProperty(nameof(Quest.goals));
+
+        var lookup = typeof(Quest.QuestGoal);
+
+        //Getting the names of all of our goals
+        m_QuestGoalType = System.AppDomain.CurrentDomain.GetAssemblies()           //load all the classes that inherits form QuestGoal Class
+            .SelectMany(assembly => assembly.GetTypes())                           // and check if the class is not abstarct and get their names and saves them
+            .Where(q => q.IsClass && !q.IsAbstract && q.IsSubclassOf(lookup))      // In to the List;
+            .Select(type => type.Name).ToList();                                   // The list will be desplayed in a dropDown menu so we can add different goal to a quest
+        
+    }
+
+    public override void OnInspectorGUI()
+    {
+        var child = m_QuestInfoProperty.Copy();
+        var depth = child.depth;
+        child.NextVisible(true);
+
+        EditorGUILayout.LabelField("Quest Info", EditorStyles.boldLabel);
+        while(child.depth > depth)
+        {
+            EditorGUILayout.PropertyField(child, true);
+            child.NextVisible(false);
+        }
+
+        child = m_QuestStatProperty.Copy();
+        depth = child.depth;
+        child.NextVisible(true);
+
+        EditorGUILayout.LabelField("Quest Reward", EditorStyles.boldLabel);
+        while(child.depth> depth)
+        {
+            EditorGUILayout.PropertyField(child, true);
+            child.NextVisible(false);
+        }
+
+        int choice = EditorGUILayout.Popup("Add a new Quest Goal", -1, m_QuestGoalType.ToArray());
+        if(choice != -1)
+        {
+            var newInstance = ScriptableObject.CreateInstance(m_QuestGoalType[choice]);
+            AssetDatabase.AddObjectToAsset(newInstance, target);
+            m_QuestGoalListProperty.InsertArrayElementAtIndex(m_QuestGoalListProperty.arraySize);
+            m_QuestGoalListProperty.GetArrayElementAtIndex(m_QuestGoalListProperty.arraySize - 1).objectReferenceValue = newInstance;
+        }
+
+        Editor editor = null;
+        int toDelete = -1;
+        for (int i = 0; i < m_QuestGoalListProperty.arraySize; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.BeginVertical();
+            var item = m_QuestGoalListProperty.GetArrayElementAtIndex(i);
+            SerializedObject SO = new SerializedObject(item.objectReferenceValue);
+
+            Editor.CreateCachedEditor(item.objectReferenceValue, null, ref editor);
+            editor.OnInspectorGUI();
+            EditorGUILayout.EndVertical();
+            if (GUILayout.Button("-", GUILayout.Width(32)))
+            {
+                toDelete = i;
+            }
+            EditorGUILayout.EndHorizontal();
+
+        }
+
+        if(toDelete != -1)
+        {
+            var item = m_QuestGoalListProperty.GetArrayElementAtIndex(toDelete).objectReferenceValue;
+            DestroyImmediate(item, true);
+            m_QuestGoalListProperty.DeleteArrayElementAtIndex(toDelete);
+            m_QuestGoalListProperty.DeleteArrayElementAtIndex(toDelete);     // for some reason we need to do it twice first is null the entry and second actually remove it!
+        }
+
+        serializedObject.ApplyModifiedProperties();
     }
 }
 #endif
