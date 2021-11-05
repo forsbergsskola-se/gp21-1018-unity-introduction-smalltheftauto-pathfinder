4b1b1bd050396f07adf1fb9eca74d7790d7bfea3
toofantah <toofan.tah@gmail.com>
2021-11-06 01:16:37 +0300

    Enemy

--- projects/SmallTheftAuto/Assets/Main/Scripts/EnemyAI.cs
+++ projects/SmallTheftAuto/Assets/Main/Scripts/EnemyAI.cs
@@ -28,7 +28,7 @@ public class EnemyAI : MonoBehaviour
 
     private void Awake()
     {
-        player = GameObject.Find("Player").transform;
+        player = GameObject.Find("Player_New").transform;
         agent = GetComponent<NavMeshAgent>();
 
     }
