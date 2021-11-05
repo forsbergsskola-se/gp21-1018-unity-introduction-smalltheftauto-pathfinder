3d344f3dff7355a0f04c467e3c5a516e24db0933
toofantah <toofan.tah@gmail.com>
2021-11-05 22:38:41 +0300

    Update

--- projects/SmallTheftAuto/Assets/Main/Scripts/Player/DeadPlayerSpawner.cs
+++ projects/SmallTheftAuto/Assets/Main/Scripts/Player/DeadPlayerSpawner.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class DeadPlayerSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject DeadCharacter;
+    //[SerializeField] private GameObject DeadCharacter;
+    Animator animator;
     void Start()
     {
         UIHealthbarScript_ML.OnPlayerDeath += PlayerDeath;
+        animator = GetComponent<Animator>();
     }
 
     private void PlayerDeath()
     {
+        /*
         GameObject holdCharacter = Instantiate(DeadCharacter);
         holdCharacter.transform.position = transform.position;
         holdCharacter.transform.rotation = transform.rotation;
-        holdCharacter.transform.Rotate(90,0,0);
+        holdCharacter.transform.Rotate(90,0,0);*/
+        animator.SetBool("isDead", true);
     }
     
    
